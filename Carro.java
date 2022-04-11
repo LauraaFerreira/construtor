5c473e917f250bfd944af91ed9ab57fd3561466d
LauraaFerreira <laurasferreira49@gmail.com>
2022-04-11 18:02:20 +0000

    2

---
+++ Carro.java
@@ -0,0 +1,21 @@
+public class Carro{
+    private String motor;
+    private String modelo;
+    private int rodas;
+    private double preco;
+
+    public Carro (){
+
+    }
+
+    Public Carro(String motor, String modelo, int rodas, double preco){
+
+    }
+
+    //faça um construtor do tipo trator, tracao
+
+    Public Trator(String motor, String modelo, int rodas, double preco);
+
+
+
+}
---
+++ Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+
+    public static void main(String[] args) {
+        Bicicleta bike = new Bicicleta();
+
+        Carro car = new Carro();
+        Carro car2 = new Carro("1.0", "up", 4, 63000);
+    }
+    
+}
